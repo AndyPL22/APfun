f259d98b62781d6848039aed88736f0358e332ca
Andrew Plowright <andrew.plowright@alumni.ubc.ca>
2017-09-25 14:54:43 -0700

    Add 'APSHPread', 'APSHPsave', 'APKML2SHP', 'APSHPfiles', 'APSHPdel' and 'APpolyMerge'

---
+++ data-raw/setSHPextensions.R
@@ -0,0 +1,5 @@
+SHPextensions <- c("shp", "shx", "dbf", "prj", "sbn", "sbx", "fbn", "fbx", "ain", "aih",
+                "ixs", "mxs", "atx", "shp.xml", "cpg", "qix")
+
+# Save data to package
+devtools::use_data(SHPextensions)
