1c05993a924cd4f5f560c37a2f90a0ec1f901f64
Andrew Plowright <andrew.plowright@alumni.ubc.ca>
2018-03-31 10:47:49 -0700

    APcontours: Added ability to control maximum number of segments per contour line. Ensured that output object's 'levels' attribute was numeric instead of a string.

--- R/APcontours.R
+++ R/APcontours.R
@@ -3,12 +3,29 @@
 #' Creates contours with rounded values
 #' @param inRaster RasterLayer. A digital surface or digital elevation model
 #' @param interval numeric. Interval for contour intervals
+#' @param max.contour.segments numeric. Maximum number of segments for a single contour line. If set to 'NULL',
+#' default value will be 25,000.
 #' @export
 
-APcontours <- function(inRaster, interval){
+APcontours <- function(inRaster, interval, max.contour.segments = NULL){
 
-contLevel <- seq(AProunder(min(raster::getValues(inRaster), na.rm = TRUE), interval, "up"),
-                 AProunder(max(raster::getValues(inRaster), na.rm = TRUE), interval, "down"),
-                 by = interval)
-raster::rasterToContour(inRaster, levels = contLevel)
+  # Seg maximum number of segments
+  oo <- options(max.contour.segments = max.contour.segments)
+  on.exit(options(oo))
+
+  # Get minimum and maximum raster values
+  rasterRange <- cellStats(inRaster, stat = "range")
+
+  # Get sequence of contour levels
+  contLevels <- seq(AProunder(rasterRange[1], interval, "up"),
+                   AProunder(rasterRange[2], interval, "down"),
+                   by = interval)
+
+  # Generate contours
+  cont <- raster::rasterToContour(inRaster, levels = contLevels)
+
+  # Make 'level' value numeric
+  cont[["level"]] <- as.numeric(cont[["level"]])
+
+  return(cont)
 }
