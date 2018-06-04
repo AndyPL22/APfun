bb56056ce039912c81741cc115c306643872c6ea
Andrew Plowright <andrew.plowright@alumni.ubc.ca>
2018-06-04 15:13:14 -0700

    Add 'APrasterFiles'

--- data-raw/SHP and Raster extensions.R
+++ data-raw/SHP and Raster extensions.R
@@ -3,3 +3,8 @@ SHPextensions <- c("shp", "shx", "dbf", "prj", "sbn", "sbx", "fbn", "fbx", "ain"
 
 # Save data to package
 devtools::use_data(SHPextensions)
+
+
+rasterExtensions <- list(tif = c("tif", "tif.aux.xml", "ovr", "tfw"))
+
+devtools::use_data(rasterExtensions)
