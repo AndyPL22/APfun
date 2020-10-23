5ef723eeb88dc0664e9ea2089603788a063621d9
Andrew Plowright <andrewandrew.plowright@alumni.ubc.ca>
2020-10-23 13:03:59 -0700

    'APpolygonize': make compatible with new OSGeo batch file locations

--- R/APpolygonize.R
+++ R/APpolygonize.R
@@ -11,7 +11,6 @@
 #' @param readToMemory logical. Read output polygons into memory as a SpatialPolygonsDataFrame
 #' @param outFile character. Optional path for saving output as an Esri Shapefile.
 #' @param OSGeoPath character. Path to the OSGeo4W installation directory
-#' @param batPath character. Path to 'gdal_polygonize.bat'. If not specified, it'll default to \code{"%OSGEO4W_ROOT%/bin/gdal_polygonize.bat"}
 #' @param connectivity numeric. Can be either set to 4 (rook's case) or 8 (queen's case)
 #'
 #' @return SpatialPolygonsDataFrame
@@ -26,66 +25,97 @@
 #'
 #' @export
 
-APpolygonize <- function(inRaster, readToMemory = TRUE, outFile = NULL, OSGeoPath = "C:/OSGeo4W64", batPath = NULL ,connectivity = 4){
+APpolygonize <- function(inRaster, readToMemory = TRUE, outFile = NULL, OSGeoPath = "C:/OSGeo4W64" ,connectivity = 4){
 
-  if(!connectivity %in% c(4,8)) stop("Connectivity can only be 4 or 8")
+  ### CHECK INPUTS ----
 
-  # Check if OSGeo root folder exists
-  if(!file.exists(OSGeoPath)) stop("Could not find folder '", OSGeoPath, "'")
+    if(!connectivity %in% c(4,8)) stop("Connectivity can only be 4 or 8")
 
-  # Get path for 'gdal_polygonize.bat'
-  if(is.null(batPath)) batPath <- file.path(OSGeoPath, "bin", "gdal_polygonize.bat")
-  if(!file.exists(batPath)) stop("Could not find required file '", batPath, "'")
+  ### SET AND CHECK PATHS ----
 
-  # Create temporary output file if needed
-  if(is.null(outFile)){
+    # Check if OSGeo root folder exists
+    if(!file.exists(OSGeoPath)) stop("Could not find folder '", OSGeoPath, "'")
 
-    outFile <- tempfile(fileext = ".shp")
-    on.exit(unlink(list.files(dirname(outFile), pattern=basename(outFile), full.names = TRUE)))
+    # Get path for 'gdal_polygonize.bat'
+    batPath <- file.path(OSGeoPath, "bin", "gdal_polygonize.bat")
 
-  }else{
+    # Newer versions of OSGeo have 'gdal_polygonize.bat' set up a bit differently.
+    # This code will check for an alternative location, which also requires additional
+    if(!file.exists(batPath)){
 
-    if(!file.exists(dirname(outFile))) stop("Could not find output folder")
-  }
+      # Try different path for 'gdal_polygonize.bat'
+      batPath <- file.path(OSGeoPath, "apps", "Python37", "Scripts", "gdal_polygonize.bat")
 
-  # Write temporary raster if needed
-  if(is(inRaster, 'Raster')){
-
-    # Check if 'inRaster' is already on the disk AND has an accepted file format
-    deniedFormat <- c("grd")
-    if(raster::fromDisk(inRaster) && !tools::file_ext(inRaster@file@name) %in% deniedFormat){
-      rastPath <- normalizePath(inRaster@file@name)
-    }else{
-      rastPath <- tempfile(fileext='.asc')
-      raster::writeRaster(inRaster, rastPath)
-      on.exit(unlink(rastPath), add = TRUE)
     }
-  }else{
-    if(is.character(inRaster)){
-      rastPath <- normalizePath(inRaster)
-      inRaster <- raster::raster(inRaster)
+    if(!file.exists(batPath)) stop("Unable to find 'gdal_polygonize.bat' file")
+
+    # Get path for batch files that set environment variables
+    envPath <- c(
+      file.path(OSGeoPath, "bin", "o4w_env.bat"),
+      file.path(OSGeoPath, "bin", "py3_env.bat")
+    )
+
+    # Create temporary output file if needed
+    if(is.null(outFile)){
+
+      outFile <- tempfile(fileext = ".shp")
+      on.exit(unlink(list.files(dirname(outFile), pattern=basename(outFile), full.names = TRUE)))
+
     }else{
-      stop("'inRaster' must be a file path (character string), or a Raster object.")
+
+      if(!file.exists(dirname(outFile))) stop("Could not find output folder")
     }
-  }
 
-  # Set connectivity switch
-  if(connectivity == 8) connectivity <- "-8 " else connectivity <- ""
+  ### WRITE TEMP RASTER ----
 
-  # Make vector of arguments
-  args <- sprintf('%1$s "%2$s" -f "%3$s" "%4$s" -q',
-                  connectivity, rastPath, "ESRI Shapefile", outFile)
+    # Write temporary raster if needed
+    if(is(inRaster, 'Raster')){
 
-  # Run OSGeo function (silence output by setting 'stdout' to TRUE)
-  outputText <- withr::with_envvar(c(OSGEO4W_ROOT = OSGeoPath), system2(batPath, args = args, stdout = TRUE))
+      # Check if 'inRaster' is already on the disk AND has an accepted file format
+      deniedFormat <- c("grd")
+      if(raster::fromDisk(inRaster) && !tools::file_ext(inRaster@file@name) %in% deniedFormat){
+        rastPath <- normalizePath(inRaster@file@name)
+      }else{
+        rastPath <- tempfile(fileext='.asc')
+        raster::writeRaster(inRaster, rastPath)
+        on.exit(unlink(rastPath), add = TRUE)
+      }
+    }else{
+      if(is.character(inRaster)){
+        rastPath <- normalizePath(inRaster)
+        inRaster <- raster::raster(inRaster)
+      }else{
+        stop("'inRaster' must be a file path (character string), or a Raster object.")
+      }
+    }
 
-  # Check if successful
-  if(!file.exists(outFile)) stop("Failed to create output file")
+  ### FORMAT ARGUMENTS ----
 
-  # Read output shapefile
-  if(readToMemory){
-    shp <- APSHPread(outFile)
-    raster::crs(shp) <- raster::crs(inRaster)
-    return(shp)
-  }
+    # Set connectivity switch
+    if(connectivity == 8) connectivity <- "-8 " else connectivity <- ""
+
+    # Make vector of arguments
+    args <- sprintf('%1$s "%2$s" -f "%3$s" "%4$s" -q',
+                    connectivity, rastPath, "ESRI Shapefile", outFile)
+
+    # Concatenate batch files and arguments
+    command <- paste(paste(shQuote(c(envPath, batPath)), collapse = " & "), args)
+
+  ### EXECUTE COMMAND ----
+
+    # Run OSGeo function (if 'show.output.on.console is set to FALSE,
+    # intern MUST be set to TRUE or it won't work for some odd reason)
+    output <- system(command, show.output.on.console = FALSE, intern = TRUE)
+
+    # Check if successful
+    if(!file.exists(outFile)) stop("Failed to create output file")
+
+  ### READ OUTPUT TO MEMORY ----
+
+    # Read output shapefile
+    if(readToMemory){
+      shp <- APSHPread(outFile)
+      raster::crs(shp) <- raster::crs(inRaster)
+      return(shp)
+    }
 }
